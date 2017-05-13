90f43a1cb5cd18be576df41ba840ad60fba1e34b
awvwgk <sehlert@uni-bonn.de>
2017-05-13 15:56:06 +0200

    Update yamsp.rb

--- yamsp.rb
+++ yamsp.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby2.1
 # YAMSP - YetAnotherMassSpectrometryParser
-# Copyright (C) 2016 Sebastian Ehlert
+# Copyright (C) 2016 2017 awvwgk
 #
 # This program is free software: you can redistribute it and/or 
 # modify it under the terms of the GNU General Public License as 
